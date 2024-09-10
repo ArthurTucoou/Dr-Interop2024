dd7f219c36b112aa32b514a694487e1fe46c4a04
laurine.rat <laurine.rat@outlook.fr>
2024-09-10 15:08:45 +0200

    sauv

--- src/routes/patients.js
+++ src/routes/patients.js
@@ -39,7 +39,7 @@ class PatientsPage extends React.Component {
     return (
       <div>
         <Overlay show={this.state.awaitingData}></Overlay>
-        {!this.props.filter && <Header title="Liste des patients"></Header>}
+        {!this.props.filter && <Header title="Patients List"></Header>}
         <PatientsListDisplay patients={patientData} loading={this.state.awaitingData} />
       </div>
     );
@@ -58,64 +58,42 @@ function doFilter(patients, filter) {
   let result = [];
   for (let patient of patients) {
     let data = patient.resource;
-
-    // Log the patient to see the structure
-    console.log("Patient data:", patient);
-
-    if (!data) {
-      console.warn("Patient resource is undefined, skipping...");
-      continue; // Skip this patient if data is undefined
-    }
-
     let match = [];
-
     if (filter.name) {
-      console.log("Checking name:", data.name);
       match.push(recursiveFind(data.name, filter.name, filter.exactMatch));
     }
-    if (filter.birthdate && data.birthDate) {
-      console.log("Checking birthdate:", data.birthDate);
+    if (filter.birthdate) {
       let isWithIn =
         filter.birthdate[0] <= moment(data.birthDate) &&
         moment(data.birthDate) <= filter.birthdate[1];
       match.push(isWithIn);
     }
-    if (filter.gender && data.gender) {
-      console.log("Checking gender:", data.gender);
+    if (filter.gender) {
       match.push(data.gender == filter.gender);
     }
-    if (filter.phone && data.telecom) {
-      console.log("Checking phone:", data.telecom);
+    if (filter.phone) {
       match.push(recursiveFind(data.telecom, filter.phone, filter.exactMatch));
     }
-    if (filter.address && data.address) {
-      console.log("Checking address:", data.address);
+    if (filter.address) {
       match.push(recursiveFind(data.address, filter.address, filter.exactMatch));
     }
-    if (filter.maritalStatus && data.maritalStatus) {
-      console.log("Checking marital status:", data.maritalStatus);
+    if (filter.maritalStatus) {
       match.push(recursiveFind(data.maritalStatus, filter.maritalStatus, filter.exactMatch));
     }
-    if (filter.id && data.id) {
-      console.log("Checking ID:", data.id);
+    if (filter.id) {
       match.push(recursiveFind(data.id, filter.id, filter.exactMatch));
     }
     if (filter.anythingElse) {
-      console.log("Checking anything else");
       match.push(recursiveFind(data, filter.anythingElse, filter.exactMatch));
     }
 
-    // Log the match results for this patient
-    console.log("Match results for this patient:", match);
-
+    // result
     if (match.every(x => x === true)) {
       result.push(patient);
     }
   }
-  console.log("Filtered result:", result);
+  console.log(result);
   return result;
 }
-
-
 
 export default PatientsPage;
